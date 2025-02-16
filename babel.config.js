ccb05ae7049da5aaee8f8fdb94782888725d6300
Baiquan Fan <1252883+bqfan@users.noreply.github.com>
2025-02-16 03:48:00 +0200

    feat: add gluestack-ui

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = function(api) {
+    api.cache(true);
+
+    return {
+        presets: [["babel-preset-expo", {
+            jsxImportSource: "nativewind"
+        }], "nativewind/babel"],
+
+        plugins: [["module-resolver", {
+            root: ["./"],
+
+            alias: {
+                "@/components/": "./src/components",
+                "@/assets": "./assets",
+                "tailwind.config": "./tailwind.config.js"
+            }
+        }]]
+    };
+};
